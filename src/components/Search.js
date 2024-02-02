089915dca8535eaa149b1d8f10e898018eee66cd
Rajesh@rh <rjakkawar@rh.com>
2024-02-02 13:26:21 +0530

    optimized code

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,43 +4,50 @@ import { IoMdArrowRoundBack } from 'react-icons/io';
 import { Redirect } from 'react-router-dom';
 
 function Search({ match }) {
+  const [data, setData] = useState([]);
+  const [back, setBack] = useState(false);
 
-	const [data, setData] = useState([])
-	useEffect(() => {
-		axios.get(`https://pixabay.com/api/?key=25850788-198f1932e82eefef8db8d5b36&q=${match.params.imgname}&image_type=photo`).then(
-			res => setData(res.data.hits)
-		)
-	}, [])
-	const [back, setBack] = useState(false);
-	if (back) {
-		return <Redirect to='/' />
-	}
-	return (
-		<section>
-			<div className='gallarey'>
-				<h3 >Gallery</h3>
-			</div>
-			<div className='row  head' style={{ margin: '28px 0px 2px 2px' }}>
-				{
-					data && data.map(imgobj =>
-						<div className='col-md-4 my-4' >
-							<div className='card index' style={{ borderRadius: '10px' }}>
-								<img className='card-img-top' src={imgobj.largeImageURL} height="250" width="250" style={{ borderTopRightRadius: '10px', borderTopLeftRadius: '10px' }} />
-								<div className='card-body' style={{ backgroundColor: '#F0EBE3', borderBottomLeftRadius: '10px', borderBottomRightRadius: '10px' }} >
-									<h5 className='card-title text-center' >{imgobj.tags}</h5>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://pixabay.com/api/?key=25850788-198f1932e82eefef8db8d5b36&q=${match.params.imgname}&image_type=photo`);
+        setData(response.data.hits);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-								</div>
-							</div>
-						</div>
-					)
-				}
-			</div>
-			<button className='back' onClick={() => setBack(true)}><IoMdArrowRoundBack /></button>
-		</section>
+    fetchData();
+  }, [match.params.imgname]);
 
+  const handleBack = () => {
+    setBack(true);
+  };
 
+  if (back) {
+    return <Redirect to='/' />;
+  }
 
-	)
+  return (
+    <section>
+      <div className='gallery'>
+        <h3>Gallery</h3>
+      </div>
+      <div className='row head'>
+        {data.map(imgobj => (
+          <div className='col-md-4 my-4' key={imgobj.id}>
+            <div className='card index'>
+              <img className='card-img-top' src={imgobj.largeImageURL} alt={imgobj.tags} height="250" width="250" />
+              <div className='card-body'>
+                <h5 className='card-title text-center'>{imgobj.tags}</h5>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button className='back' onClick={handleBack}><IoMdArrowRoundBack /></button>
+    </section>
+  );
 }
 
-export default Search
+export default Search;
